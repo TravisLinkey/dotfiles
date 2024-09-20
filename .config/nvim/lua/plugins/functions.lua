600e62149fedef76f3cee261fececfe2269b8e9d
LiftsKit LLC <liftskit@gmail.com>
2024-09-20 09:54:14 -0700

    updated printing scripts

--- .config/nvim/lua/plugins/functions.lua
+++ .config/nvim/lua/plugins/functions.lua
@@ -5,7 +5,7 @@ vim.api.nvim_create_user_command("JavaScriptCode", function()
   local append_data = function(_, data)
     local r, _ = unpack(vim.api.nvim_win_get_cursor(0))
     if data then
-      vim.api.nvim_buf_set_lines(0, r, r, false, data)
+      vim.api.nvim_buf_set_lines(0, r-1, r, false, data)
     end
   end
   vim.fn.jobstart(command, {
@@ -20,7 +20,7 @@ vim.api.nvim_create_user_command("PythonCode", function()
   local append_data = function(_, data)
     local r, _ = unpack(vim.api.nvim_win_get_cursor(0))
     if data then
-      vim.api.nvim_buf_set_lines(0, r, r, false, data)
+      vim.api.nvim_buf_set_lines(0, r-1, r, false, data)
     end
   end
   vim.fn.jobstart(command, {
--- Scripts/print_javascript_code.sh
+++ Scripts/print_javascript_code.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 echo '```javascript```'
-printf "\n"
 
