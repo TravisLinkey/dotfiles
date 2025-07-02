9e0f08544cfad36d9639c00b0460a3e14d0f786c
Travis Linkey <travis.linkey@servicecore.com>
2025-07-02 08:34:04 -0700

    chore: fix files

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "preservim/vim-markdown",
+    ft = "markdown",
+    config = function()
+      -- Configure vim-markdown for better checkbox support
+      vim.g.vim_markdown_folding_disabled = 1
+      vim.g.vim_markdown_override_foldtext = 0
+      vim.g.vim_markdown_folding_level = 6
+      vim.g.vim_markdown_toc_autofit = 1
+      vim.g.vim_markdown_math = 1
+      vim.g.vim_markdown_frontmatter = 1
+      vim.g.vim_markdown_strikethrough = 1
+      vim.g.vim_markdown_autowrite = 1
+      vim.g.vim_markdown_new_list_item_indent = 2
+      vim.g.vim_markdown_no_extensions_in_markdown = 1
+      vim.g.vim_markdown_conceal = 0
+      vim.g.vim_markdown_conceal_code_blocks = 0
+      
+      -- Disable checkbox conceal specifically
+      vim.g.vim_markdown_conceal = 0
+      vim.g.vim_markdown_conceal_code_blocks = 0
+      
+      -- Additional settings to prevent checkbox rendering issues
+      vim.g.vim_markdown_auto_insert_bullets = 0
+      vim.g.vim_markdown_new_list_item_indent = 0
+      vim.g.vim_markdown_folding_style_pythonic = 1
+    end
+  },
+  {
+    "mzlogin/vim-markdown-toc",
+    ft = "markdown",
+    config = function()
+      -- Configure markdown TOC
+      vim.g.vmt_auto_update_on_save = 0
+      vim.g.vmt_dont_insert_fence = 1
+    end
+  }
+} 
