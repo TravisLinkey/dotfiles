02538a27e12228a18ac7b400f6ec82b2a6cd5ae4
Travis Linkey <travis.linkey@servicecore.com>
2025-07-14 18:45:10 -0700

    chore: added personal project

--- zsh/aliases/projects.zsh
+++ zsh/aliases/projects.zsh
@@ -4,10 +4,12 @@
 alias Notes="cd ~/Documents/Obsidian/Travis/obsidian && nvim ."
 alias chat="cd ~/Projects/Elixir/workspace/chat_app"
 alias dot="cd ~/dotfiles && nvim ."
+alias ca="cd ~/Projects/Elixir/workspace/chat_app/"
 
 
 ## Docket workspaces
 alias dk="cd ~/Projects/ServiceCore/docket"
 alias dp="cd ~/Projects/ServiceCore/docket-platform"
-alias be="cd /apps/docket-backend"
-alias api="cd /apps/docket-api"
+alias be="cd apps/docket-backend"
+alias api="cd apps/docket-api"
+
