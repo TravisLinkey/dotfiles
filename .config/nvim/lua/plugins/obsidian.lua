46ba2bc48e03146b101993c4333bae43ee3a818a
Travis Linkey <travis.linkey@servicecore.com>
2025-08-01 19:54:33 -0700

    chore: frontmatter working

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -2,15 +2,11 @@ return {
   "epwalsh/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
+  -- Only load for files in your obsidian vault, not all markdown files
+  event = {
+    "BufReadPre ~/Documents/Obsidian/Travis/obsidian/**/*.md",
+    "BufNewFile ~/Documents/Obsidian/Travis/obsidian/**/*.md",
+  },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
@@ -32,28 +28,24 @@ return {
       return title
     end,
 
+    -- Configure frontmatter behavior
+    note_frontmatter_func = function(note)
+      -- This function is called when saving notes
+      if note.metadata ~= nil then
+        return note.metadata
+      end
+      
+      -- Return default frontmatter if none exists
+      return {
+        created = os.date("%Y-%m-%d %H:%M:%S"),
+        modified = os.date("%Y-%m-%d %H:%M:%S"),
+        tags = {},
+      }
+    end,
+
     -- see below for full list of options 👇
   },
   config = function(_, opts)
     require("obsidian").setup(opts)
-    
-    -- Add custom checkbox toggle mapping that works with completion plugin
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = "markdown",
-      callback = function()
-        -- Ensure buffer is modifiable
-        vim.bo.modifiable = true
-        vim.bo.readonly = false
-        
-        -- Map leader + ; to toggle checkbox
-        vim.keymap.set("n", "<leader>;", function()
-          if vim.bo.modifiable then
-            require("obsidian").util.toggle_checkbox()
-          else
-            vim.notify("Buffer is not modifiable", vim.log.levels.WARN)
-          end
-        end, { buffer = true, noremap = true })
-      end,
-    })
   end,
 }
--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -23,20 +23,20 @@ vim.api.nvim_create_user_command('MoveFile', function()
 end, {})
 
 -- Safe checkbox toggle function
-vim.api.nvim_create_user_command('ToggleCheckbox', function()
-  -- Ensure buffer is modifiable
-  vim.bo.modifiable = true
-  vim.bo.readonly = false
+-- vim.api.nvim_create_user_command('ToggleCheckbox', function()
+--   -- Ensure buffer is modifiable
+--   vim.bo.modifiable = true
+--   vim.bo.readonly = false
   
-  -- Try to toggle checkbox safely
-  local success, result = pcall(function()
-    require("obsidian").util.toggle_checkbox()
-  end)
+--   -- Try to toggle checkbox safely
+--   local success, result = pcall(function()
+--     require("obsidian").util.toggle_checkbox()
+--   end)
   
-  if not success then
-    vim.notify("Failed to toggle checkbox: " .. tostring(result), vim.log.levels.ERROR)
-  end
-end, {})
+--   if not success then
+--     vim.notify("Failed to toggle checkbox: " .. tostring(result), vim.log.levels.ERROR)
+--   end
+-- end, {})
 
 -- Ensure markdown buffers are modifiable for obsidian.nvim
 vim.api.nvim_create_autocmd("FileType", {
@@ -53,6 +53,56 @@ vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function()
     vim.bo.modifiable = true
     vim.bo.readonly = false
+  end,
+})
+
+-- Add frontmatter on save for obsidian vault files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.md",
+  callback = function()
+    local bufname = vim.api.nvim_buf_get_name(0)
+    
+    -- Only process files in the obsidian vault
+    if bufname:match("Documents/Obsidian/Travis/obsidian") then
+      vim.notify("Processing obsidian file: " .. bufname, vim.log.levels.INFO)
+      
+      local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+      local has_frontmatter = false
+      
+      -- Check if file already has frontmatter
+      if #lines > 0 and lines[1] == "---" then
+        for i = 2, #lines do
+          if lines[i] == "---" then
+            has_frontmatter = true
+            break
+          end
+        end
+      end
+      
+      -- Add frontmatter if it doesn't exist
+      if not has_frontmatter then
+        vim.notify("Adding frontmatter to: " .. bufname, vim.log.levels.INFO)
+        
+        -- Get the filename without extension for the ID
+        local filename = vim.fn.fnamemodify(bufname, ":t:r")
+        
+        local metadata = {
+          "---",
+          "id: " .. filename,
+          "created: " .. os.date("%Y-%m-%d %H:%M:%S"),
+          "modified: " .. os.date("%Y-%m-%d %H:%M:%S"),
+          "tags: []",
+          "aliases: []",
+          "---",
+          ""
+        }
+        
+        -- Insert metadata at the beginning
+        vim.api.nvim_buf_set_lines(0, 0, 0, false, metadata)
+      else
+        vim.notify("File already has frontmatter: " .. bufname, vim.log.levels.INFO)
+      end
+    end
   end,
 })
 
