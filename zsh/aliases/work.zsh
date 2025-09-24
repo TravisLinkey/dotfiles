2d9cabadcc79a832567c7c5960c169424606ca2d
Travis Linkey <travis.linkey@servicecore.com>
2025-09-24 08:49:10 -0700

    chore: add more project aliases

--- zsh/aliases/work.zsh
+++ zsh/aliases/work.zsh
@@ -1,10 +1,11 @@
 
 ## Docket workspaces
+alias api="cd apps/docket-api"
+alias be="cd apps/docket-backend"
+alias cp="cd ~/Projects/ServiceCore/Docket-Customer-Portal"
 alias dk="cd ~/Projects/ServiceCore/docket"
 alias dp="cd ~/Projects/ServiceCore/docket-platform"
-alias cp="cd ~/Projects/ServiceCore/Docket-Customer-Portal"
-alias be="cd apps/docket-backend"
-alias api="cd apps/docket-api"
+alias sc="cd ~/Projects/ServiceCore/Survcart"
 
 alias init="bash ~/dotfiles/zsh/aliases/copy_env.sh"
 
